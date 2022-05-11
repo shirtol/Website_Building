74581ea4b3880c64d2179329883cfb8342b97cea
ariw96 <ariw96@gmail.com>
2022-05-11 15:18:21 +0300

    delete row feature

--- script/script.js
+++ script/script.js
@@ -1,15 +1,14 @@
-const getData = async () => {
+const getData = async() => {
     const res = await fetch(
         "https://capsules-asb6.herokuapp.com/api/teacher/mordi"
     );
     const data = await res.json();
-    console.log(data);
+    console.table(data);
 };
-
 getData();
 
-const students = [
-    {
+
+const students = [{
         name: "Tofik",
         lastName: "Saliba",
         id: "1",
@@ -29,11 +28,14 @@ const students = [
 const addRow = (student, container) => {
     const studentArr = Object.entries(student);
     console.log("array", studentArr);
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
     const row = document.createElement("div");
     row.classList.add("row");
+    btn.textContent = "delete row"
+    row.appendChild(btn)
     studentArr.forEach((e) => {
         console.log("key", e);
-
         const cell = document.createElement("div");
         cell.classList.add("cell");
         cell.textContent = e[1];
@@ -43,11 +45,18 @@ const addRow = (student, container) => {
     container.appendChild(row);
 };
 
-addRow(
-    { 0: "name", 1: "last name", 2: "ID" },
+addRow({ 0: "name", 1: "last name", 2: "ID" },
     document.querySelector(".container")
 );
 
 students.forEach((student) => [
     addRow(student, document.querySelector(".container")),
 ]);
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(function(currentBtn) {
+    currentBtn.addEventListener('click', deleteRow)
+})
+
+function deleteRow(e) {
+    e.target.parentElement.remove()
+};
